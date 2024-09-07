7e87a291ddb085b216b780e06d38fc2f151bfbac
nayab <vinabi2k4@gmail.com>
2024-09-07 13:52:38 +0500

    Update urls.py

--- remedi/urls.py
+++ remedi/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('generate_key/', views.generate_key_view, name='generate_key'),
     path('encrypt/', views.encrypt_view, name='encrypt'),
     path('decrypt/', views.decrypt_view, name='decrypt'),
+    path('payment/', views.payment_view, name='payment'),
+    path('create-checkout-session/', views.create_checkout_session, name='create_checkout_session'),
+    path('success/', views.success_view, name='success'),
+    path('cancel/', views.cancel_view, name='cancel'),
 ]
